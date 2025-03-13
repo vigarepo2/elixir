7008c1b862aa935a18d5dbfe117ad2ae28c246d5
Viga Bots <133951124+vigarepo2@users.noreply.github.com>
2025-03-13 22:47:53 +0530

    Create mix.exs

---
+++ mix.exs
@@ -0,0 +1,26 @@
+defmodule HelloWorld.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :hello_world,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      mod: {HelloWorld.Application, []},
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:plug_cowboy, "~> 2.5"}
+    ]
+  end
+end
